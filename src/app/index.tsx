9b5d66479d056d5c04aa5129e8cbd384d965dc31
Bognar Junior <bognar_junior@yahoo.com.br>
2025-01-09 16:16:35 -0300

    Update app.tsx
    Create skeleton in app.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,13 +8,28 @@ import Animated,
   withTiming 
 } from 'react-native-reanimated';
 
+import { Skeleton } from 'moti/skeleton';
+
 import { styles } from './styles';
+import { theme } from '@/theme';
+
+const { colors } = theme;
 
 export default function Splash() {
   const logoScale = useSharedValue(1);
   const logoPositionY = useSharedValue(0);
   
   const dimensions = useWindowDimensions();
+
+  const skeletonColor = [
+    colors.Gray[600],
+    colors.Gray[700],
+    colors.Gray[800],
+    colors.Black,
+    colors.Gray[800],
+    colors.Gray[700],
+    colors.Gray[600],
+  ]
 
   const logoAnimatedStyle = useAnimatedStyle(() => ({
     transform: [
@@ -39,6 +54,40 @@ export default function Splash() {
     );
   };
 
+  function boxes(colums: "right" | "left") {
+    const rest = colums === "left" ? 0 : 1;
+    return Array.from({length: 10})
+    .filter((_, index) => index % 2 === rest)
+    .map((_, index) => {
+      const height = index %2 === (colums === "left" ? 0 : 1) ? 200 : 300;
+      return (
+        <Animated.View 
+          key={index}
+          style={[styles.box, { height }]}
+        >
+          <Skeleton 
+            colors={skeletonColor}
+            width="100%"
+            height={height}
+          />
+        </Animated.View>
+      )
+    })
+  }
+
+  function filters(){
+    return Array.from({length: 10})
+    .map((_, index) => (
+      <Skeleton 
+        key={index}
+        colors={skeletonColor}
+        width={60}
+        height={26}
+        radius={6}
+      />
+    ))
+  }
+
   useEffect(() => {
     logoAnimation();
   },[])
@@ -48,6 +97,18 @@ export default function Splash() {
         source={require('@/assets/logo.png')} 
         style={[styles.image, logoAnimatedStyle]} 
       />
+
+      <View style={styles.filters}>
+        {filters()}
+      </View>
+      <View style={styles.boxes}>
+        <View style={styles.column}>
+          {boxes("left")}
+        </View>
+        <View style={styles.column}>
+          {boxes("right")}
+        </View>
+      </View>
     </View>
   )
 }
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -8,10 +8,32 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.Black,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    paddingHorizontal: 12
   },
   image: {
     width: 64,
     height: 64
+  },
+  boxes: {
+    flex: 1,
+    flexDirection: "row",
+    width: "100%",
+    gap: 12
+  },
+  box: {
+    width: "100%",
+    backgroundColor: colors.Gray[600],
+    borderRadius: 16
+  },
+  column:{
+    flex: 1,
+    gap: 12
+  },
+  filters: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 16,
+    marginBottom: 12
   }
 });
