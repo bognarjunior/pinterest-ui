053f8fa4d126b624a0c16b0b4a83812d69d7fbe6
Bognar Junior <bognar_junior@yahoo.com.br>
2025-01-09 13:31:24 -0300

    Create components
    Create components Post and Posts

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+import Post from '@/components/Post';
+
+export default function Posts() {
+  return (
+    <View>
+      <Post />
+    </View>
+  )
+}
